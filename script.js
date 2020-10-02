1bc29d5fee88f2e9b6c5321bde4b52f9f8ecbb8c
MarceloCLopes <marc01mcl@gmail.com>
2020-10-02 09:23:24 -0300

    Definindo cores que irão  ser mostradas para nós

--- script.js
+++ script.js
@@ -10,4 +10,15 @@ let score = 0;
 const green = document.querySelector('.green');
 const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
-const blue = document.querySelector('.blue')
+const blue = document.querySelector('.blue');
+
+let shuffleOrder = () => {
+  let colorOrder = Math.floor(Math.random() * 4);
+  order[order.length] = colorOrder;
+  clickedOrder = [];
+
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
+  }
+}
